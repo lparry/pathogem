7a63e0a1859ff18fa89a2d1729d5dbb9d2324305
Lucas Parry <lucas@envato.com>
2011-05-29 20:55:05 +1000

    stupidly basic CLI option parsing

--- lib/pathogem.rb
+++ lib/pathogem.rb
@@ -1,5 +1,18 @@
 require "pathogem/version"
 
 module Pathogem
-  # Your code goes here...
+  def self.install(gem_thingy)
+  end
+
+  def self.uninstall(gem_thingy)
+  end
+
+  def self.update(gem_thingy)
+  end
+
+  def self.help_message
+    <<-HELP
+Oh snap, you ran this before I got round to sorting out help. I guess you'll have to dig into the source.
+    HELP
+  end
 end
