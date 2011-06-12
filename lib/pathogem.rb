9f92cba7957a9abbb57521955c3a238e705bbbb5
Lucas Parry <lucas@envato.com>
2011-06-12 19:15:20 +1000

    some crappy help

--- lib/pathogem.rb
+++ lib/pathogem.rb
@@ -71,7 +71,13 @@ module Pathogem
 
   def self.help_message
     <<-HELP
-Oh snap, you ran this before I got round to sorting out help. I guess you'll have to dig into the source.
+Oh snap, you ran this before I got round to sorting out help. I guess you'll have to dig into the source. Or you could try one of the following
+  pathogem install plugin
+  pathogem update plugin
+  pathogem update --all
+  pathogem uninstall plugin
+  pathogem list
+  pathogem search
     HELP
   end
 
